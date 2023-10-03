398f51c0a2fc887a01a23b6de5a0517626a8838d
Joshua Sample <josh.sample@ware2go.com>
2023-10-03 18:50:26 -0400

    lightweight comments micro for service play

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const APP_PORT = 4001;
+// no db for toy app
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+  
+  res.status(201).send(comments);
+});
+
+app.listen(APP_PORT, () => {
+  console.log(`Listening on ${APP_PORT}`);
+});
