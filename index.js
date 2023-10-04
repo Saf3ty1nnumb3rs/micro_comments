fbfb38b1f34082c263d7eb5edc70d6cbce6737e3
Joshua Sample <josh.sample@ware2go.com>
2023-10-03 20:40:31 -0400

    set up events post

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
@@ -11,21 +12,33 @@ const APP_PORT = 4001;
 // no db for toy app
 const commentsByPostId = {};
 
+// POSTS/COMMENTS
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
 
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: { id: commentId, content, postId: req.params.id },
+  });
   
   res.status(201).send(comments);
 });
 
+// EVENTS
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+});
 app.listen(APP_PORT, () => {
   console.log(`Listening on ${APP_PORT}`);
 });
